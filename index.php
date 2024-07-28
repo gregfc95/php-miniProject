ac843eeeac89aa7d8c4c64a477e2791d7b8b463e
gregfc95 <josegfc95@gmail.com>
2024-07-28 20:51:12 -0300

    poster of next title

--- index.php
+++ index.php
@@ -2,22 +2,25 @@
 
 const API_URL = "https://whenisthenextmcufilm.com/api";
 #Inicializar una nueva sesion de cURL; ch = cURL handle
-$ch = curl_init(API_URL);
-//Indicar que queremos recibir el resultado de la peticion y no mostrar en pantalla
-//ch es el cURL handle
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//$ch = curl_init(API_URL);
+#Indicar que queremos recibir el resultado de la peticion y no mostrar en pantalla
+#ch es el cURL handle
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 /* Ejecutar la peticion
 y guardamos el resultado
 */
-
 #Alternativa utilizar file_get_contents
-# $result = file_get_contents(API_URL); //Si solo quieres un get
-$result = curl_exec($ch);
+function get_data(string $url) {
+    $result = file_get_contents(API_URL); //Si solo quieres un get
+    $data = json_decode($result,true);
+    return $data;
+}
+//$result = curl_exec($ch);
 #Transformamos el json a un array asociativo y guardamos en result
-$data = json_decode($result,true);
 
 #Cerramos la conexion cURL
-curl_close($ch);
+//curl_close($ch);
+$data = get_data(API_URL);
 ?>
 
 <!DOCTYPE html>
@@ -26,7 +29,7 @@ curl_close($ch);
     <meta charset="UTF-8">
     <meta name="description" content="Next Marvel movie" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Next Marvel movie</title>
+    <title>Next Marvel title</title>
     <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"
@@ -49,6 +52,13 @@ curl_close($ch);
         <h3><?= $data["title"]; ?> premieres in <?= $data["days_until"]; ?> days</h3>
         <p>Release Date: <?= $data["release_date"]; ?></p>
         <p>Next... "<?= $data["following_production"]["title"]; ?>", in <?= $data["following_production"]["days_until"]; ?> days</p>
+        <img 
+            src="<?= $data["following_production"]["poster_url"]; ?>" 
+                width="100" 
+                alt="Poster of <?= $data["title"]; ?>" 
+            style="border-radius: 16px"
+            />
+        </section>
     </main>
     
 </body>
@@ -65,7 +75,7 @@ curl_close($ch);
     }
 
     img {
-        margion: 0 auto
+        margin: 0 auto
     }
 
     section {
