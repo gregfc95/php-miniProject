a435b75e97b050418db79985725a31a6d31b6aca
gregfc95 <josegfc95@gmail.com>
2024-07-28 22:25:00 -0300

    creating functions and cleaning the code

--- index.php
+++ index.php
@@ -1,26 +1,11 @@
 <?php 
 
 const API_URL = "https://whenisthenextmcufilm.com/api";
-#Inicializar una nueva sesion de cURL; ch = cURL handle
-//$ch = curl_init(API_URL);
-#Indicar que queremos recibir el resultado de la peticion y no mostrar en pantalla
-#ch es el cURL handle
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-/* Ejecutar la peticion
-y guardamos el resultado
-*/
-#Alternativa utilizar file_get_contents
-function get_data(string $url) {
-    $result = file_get_contents(API_URL); //Si solo quieres un get
-    $data = json_decode($result,true);
-    return $data;
-}
-//$result = curl_exec($ch);
-#Transformamos el json a un array asociativo y guardamos en result
+require 'functions.php';
 
-#Cerramos la conexion cURL
-//curl_close($ch);
 $data = get_data(API_URL);
+$untilMessage = get_until_message($data["days_until"]);
+$untilMessageNext = get_until_message($data["following_production"]["days_until"]);
 ?>
 
 <!DOCTYPE html>
@@ -37,9 +22,6 @@ $data = get_data(API_URL);
 </head>
 <body>
     <main>
-<!--         <pre style="font-size: 12px; overflow: scroll;">
-        #    <?php   var_dump($data); ?>
-        </pre> -->
         <section>
             <img 
             src="<?= $data["poster_url"]; ?>" 
@@ -49,9 +31,9 @@ $data = get_data(API_URL);
             />
         </section>
         <hgroup>
-        <h3><?= $data["title"]; ?> premieres in <?= $data["days_until"]; ?> days</h3>
+        <h3><?= $data["title"]; ?> - <?= $untilMessage ?></h3>
         <p>Release Date: <?= $data["release_date"]; ?></p>
-        <p>Next... "<?= $data["following_production"]["title"]; ?>", in <?= $data["following_production"]["days_until"]; ?> days</p>
+        <p>Next... "<?= $data["following_production"]["title"]; ?>" - <?= $untilMessageNext ?></p>
         <img 
             src="<?= $data["following_production"]["poster_url"]; ?>" 
                 width="100" 
